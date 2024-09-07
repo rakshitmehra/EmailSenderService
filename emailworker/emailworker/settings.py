7740d95a9f15f75c89df6528602acfdc01d9d3c7
Rakshit Mehra <rakshitmehra88@gmail.com>
2024-09-07 13:18:26 +0530

    Update settings.py

--- emailworker/emailworker/settings.py
+++ emailworker/emailworker/settings.py
@@ -125,8 +125,8 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_BROKER_URL = 'redis://email-service.redis.cache.windows.net:6379/0'
+CELERY_RESULT_BACKEND = 'redis:email-service.redis.cache.windows.net:6379/0'
 
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
