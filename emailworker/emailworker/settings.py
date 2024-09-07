dc59cfef45c5afc54aa80f9b363c52b2344a0fb4
Rakshit Mehra <rakshitmehra88@gmail.com>
2024-09-07 12:20:29 +0530

    Update settings.py

--- emailworker/emailworker/settings.py
+++ emailworker/emailworker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-nprkgwo%n=%mdmc-wdda)-s%1@56%nibhh@=_8$)oj8n#jnx_w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
