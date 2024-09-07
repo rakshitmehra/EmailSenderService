fb7f97c60851b861a91a4b47ab3448b150411504
Rakshit Mehra <rakshitmehra88@gmail.com>
2024-09-08 00:40:19 +0530

    Update settings.py

--- emailworker/emailworker/settings.py
+++ emailworker/emailworker/settings.py
@@ -125,8 +125,8 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CELERY_BROKER_URL = 'CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_BROKER_URL ='redis://localhost:6379/0'
+CELERY_RESULT_BACKEND ='redis://localhost:6379/0'
 
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
